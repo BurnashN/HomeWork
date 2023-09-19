11a88a144140ab45db6707a1120d249eaf85563e
BurnashN <142603803+BurnashN@users.noreply.github.com>
2023-09-19 20:42:10 +0700

    Add files via upload

---
+++ done11/js_lesson77/main.js
@@ -0,0 +1,17 @@
+// + - * / ** %
+let sum = 3 + 1
+console.log(sum)
+let minus = 10-4
+console.log(minus)
+let divig = minus / sum
+console.log(divig**2)
+
+
+
+
+
+
+
+
+// дз================================================================
+
